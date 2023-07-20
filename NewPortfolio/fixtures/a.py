052a3a8a4ed04dbb622ad0cd81f606e909d7a1bd
hliza84 <hovhannisyanliza84@gmail.com>
2023-07-20 14:33:55 +0400

    Added cheanges

--- NewPortfolio/fixtures/a.py
+++
@@ -1,9 +0,0 @@
-import chardet
-
-with open("file.txt", "rb") as file:
-    raw_data = file.read()
-    result = chardet.detect(raw_data)
-    encoding = result["encoding"]
-
-with open("file.txt", "r", encoding=encoding) as file:
-    data = file.read()
